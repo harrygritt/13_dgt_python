cacb2f43a2667b5f8219a13b7d28e5a2a9e286c2
Harry Gritt <harry.gritt@wbhs.school.nz>
2024-02-12 15:05:05 +1300

    added start menu options

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import requests
 import os
 
 # Arrays
-array_difficulty = ["Easy","Medium","Hard","Any Difficulty"]
+difficulty = ["Easy","Medium","Hard","Any Difficulty"]
+start_options = ["Information","Players","Category","Difficulty","Exit"]
 
 # API links
 api_url = "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy"
@@ -14,20 +15,18 @@ category_options = requests.get(category_url).json()
 category_options = category_options.get("trivia_categories")
 PRINTING_WIDTH = 50
 DIVIDER_CHARACTER = "="
-INVALID_MENU_ENTRY = "Please select a valid option (select number)"
+INVALID_MENU_ENTRY = "Please select a valid option by entering a valid number"
 
 
+# Show category options 
 def category_menu():
+
     # Print category options
     print_array([category.get('name') for category in category_options])
+
     # Ask user to select category
-    
-
-
-# Printing divider function
-def divider():
-    # Printing divider
-    print(PRINTING_WIDTH * DIVIDER_CHARACTER)
+    category_index = input_checking("Which category would you like to be tested on?:" , category_options)
+    print(category_options[category_index - 1])
 
 
 # Clearing screen function
@@ -38,33 +37,37 @@ def clear():
 
 # Format single level list function
 def print_array(items: list):
+
     # Print the divider
     print(PRINTING_WIDTH * DIVIDER_CHARACTER)
 
-    # For every index for the item in the list
+    # Print the items in array as well as their index
     for index in range(len(items)):
-        # Print index and item
         print(f"[{index + 1}] {items[index]}")
 
     print(PRINTING_WIDTH * DIVIDER_CHARACTER)
 
 
 # Error checking function
-def input_checking(prompt: str, array: list, start_index: int = 1, error_message=INVALID_MENU_ENTRY):
+def input_checking(prompt: str, array: list, start_index: int = 1, error_message = INVALID_MENU_ENTRY):
+
     # Loop while input is invalid
     while True:
         try:
             input_index = int(input(prompt))
         except ValueError:
-            print(INVALID_MENU_ENTRY, "by entering a valid number")
+            print(INVALID_MENU_ENTRY)
             continue
-
         # Check if option is in chosen list/menu, if not, display error message and loop
         if input_index not in range(start_index, len(array) + 1):
             print(error_message)
         else:
             return input_index
 
+
+# Show start menu
+def start_menu():
+    print_array(start_options)
 
 
 category_menu()
